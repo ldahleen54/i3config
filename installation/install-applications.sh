e1fa0abc640ce66fa958cdf9ae0e124fe8a26695
ldahleen54 <ldahleen54@gmail.com>
2020-07-18 11:35:03 -0500

    Added installation folder to install applications

---
+++ installation/install-applications.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+#
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+
+sh applications/install-spotify.sh
+
+sh applications/install-variety.sh
+
+sh applications/install-google-chrome.sh
+
+sh applications/install-neofetch.sh
+
+sh applications/install-zsh.sh
+
+
+echo "#########################################################"
+echo "Applications have been  installed"
+echo "#########################################################"
+sleep 1
