f583cf50cf651325f7933c48917a9a91e9df8b79
ldahleen54 <ldahleen54@gmail.com>
2020-07-18 14:47:32 -0500

    Added more applications for installation

---
+++ installation/applications/install-terminal-applications.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+##################################################################################################################
+# Author 	: 	Luke Dahleen
+# Website 	: 	http://www.lukedahleen.com
+##################################################################################################################
+
+# installing terminal appliations and other small utilities
+sudo apt install bat -y
+echo "Bat is installed"
+sudo apt-get install cava -y 
+echo "Cava is installed"
+sudo apt-get install cmatrix -y
+echo "Cmatrix is installed"
+sudo apt-get install -y unclutter
+echo "Unclutter is installed"
+sudo apt-get install -y htop
+echo "Htop is installed"
+sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
+sudo chmod a+rx /usr/local/bin/youtube-dl
+echo "Youtube-dl is installed"
