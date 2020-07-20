d294060c3cd4716363f3c38d50d3baaa71849ff5
Luke <ldahleen54@gmail.com>
2020-07-19 20:36:35 -0500

    Removed dropbox. Removed neofetch unused script. Using snap to install spotify.

--- installation/install-applications.sh
+++ installation/install-applications.sh
@@ -3,7 +3,7 @@ set -e
 
 sh applications/install-biogenesis.sh
 sh applications/install-discord.sh
-sh applications/install-dropbox.sh
+#sh applications/install-dropbox.sh
 sh applications/install-kazam.sh
 sh applications/install-nodejs.sh
 sh applications/install-terminal-applications.sh
@@ -12,7 +12,6 @@ sh applications/install-vscode.sh
 sh applications/install-spotify.sh
 sh applications/install-variety.sh
 sh applications/install-google-chrome.sh
-sh applications/install-neofetch.sh
 sh applications/install-zsh.sh
 sh applications/install-steam.sh
 
